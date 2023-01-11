16ea490be53eab0245d853f14d59b54de93a0fdc
bmappa1342 <bmappaisidk@groupeisi.com>
2023-01-11 20:33:20 +0000

    creation du projet ; mise en place du NavBar; creation du carousel

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-app>
+    <v-main>
+      <HelloWorld/>
+      <v-content>
+      <HeroPhoto/>
+    </v-content>
+    </v-main>
+   
+  </v-app>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import HeroPhoto from './components/HeroPhoto.vue'
+
+export default {
+  name: 'App',
+
+  components: {
+    HelloWorld,
+    HeroPhoto
+  },
+
+  data: () => ({
+    //
+  }),
+}
+</script>
